423c87569b6243b82f23a550e7033241a563ab2b
camachoo18 <estivencamachops@gmail.com>
2024-11-04 10:42:35 +0100

    #hacer ping a dominios

--- server.js
+++ server.js
@@ -2,20 +2,27 @@ const express = require("express")
 const { exec } = require('child_process');
 
 
-const comando = "figlet hola mundo";
+//const comando = "figlet hola mundo";
 const app = express()
 const port = 3000
 
 
-
+app.use(express.static("public"))
 
 app.get("/", (req, res) =>{
     exec (comando, (error, stdout, stderr)=>{
-        console.log(error, stdout, stderr)
-        console.log(`Stdout: ${stdout}`);
         res.send(`<pre>${stdout}</pre>`);
     })
 })
+
+app.get("/ping",(req,res)=>{
+    const dominio = req.query.dominio || 'google.com'; 
+    const comando = `ping -c 4 ${dominio}`;
+    exec (comando, (error, stdout, stderr)=>{
+        res.send(`<pre>${stdout}</pre>`);
+    })
+})
+
 
 app.listen(port, ()=>{
     console.log("Hola estas en el puerto 3000")
