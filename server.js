84887038f2d37bff3be360a7e65b0d490a0aed57
Estiven <estivencamachops@gmail.com>
2024-11-04 16:34:27 +0100

    aplico shell quote para evitar problemas de inyeccion por parte del usuario

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const { exec } = require('child_process');
+const { quote } = require('shell-quote');
+
 
 
 //const comando = "figlet hola mundo";
@@ -15,13 +17,17 @@ app.get("/", (req, res) =>{
     })
 })
 
-app.get("/ping",(req,res)=>{
-    const dominio = req.query.dominio || 'google.com'; 
-    const comando = `ping -c 4 ${dominio}`;
-    exec (comando, (error, stdout, stderr)=>{
-        res.send(`<pre>${stdout}</pre>`);
-    })
-})
+app.get("/ping", (req, res) => {
+    const dominio = req.query.dominio || 'google.com';
+    const comando = `ping -n 4 ${quote([dominio])}`;
+
+    //ping -n 4 ${dominio}`   Comando para Windows
+    //ping -c 4 ${dominio}`;  Comando para Unix/Linux
+
+    exec(comando, (error, stdout) => {
+        res.send(`<pre>${error ? `Error ejecutando ping: ${error.message}` : stdout}</pre>`);
+    });
+});
 
 
 app.listen(port, ()=>{
